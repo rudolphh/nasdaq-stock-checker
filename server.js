76c870364ed42fd35741d92f161b2f82576de43c
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-07-18 12:55:39 -0700

    Add content security policy

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 'use strict';
 
+var dotenv      = require('dotenv').load();
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+var helmet      = require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
+
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    styleSrc: ["'self'"],
+    scriptSrc: ["'self'",'https://code.jquery.com'],
+    imgSrc: ["'self'", 'https://hyperdev.com', 'http://glitch.com/favicon-app.ico']
+  }
+}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -27,9 +39,9 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
