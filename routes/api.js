b9243e873e1a14c1ca64acc43bf03610ea443075
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-07-20 13:00:38 -0700

    Get stock prices using yahoo finance api

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,7 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
+var request = require('request');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
@@ -18,6 +19,25 @@ module.exports = function (app) {
   app.route('/api/stock-prices')
     .get(function (req, res){
       
+       var stock =  encodeURIComponent(req.query.stock);
+       var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+       ip = ip.split(',')[0];
+       console.log(ip);
+
+       request("https://finance.google.com/finance/info?q=NASDAQ%3a"+stock,
+        function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body.substring(3))[0];
+            var stockDoc = {
+              stockData: {
+                stock: data.t,
+                price: data.l,
+                likes: 0
+              }
+            }
+            res.json(stockDoc);
+          }
+        });
     });
-    
+
 };
