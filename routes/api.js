f52ea1b16c5c67fc3170967999f9d7b6e5b2c25b
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-07-23 12:49:59 -0700

    Async calls using callbacks for one or two stocks

--- routes/api.js
+++ routes/api.js
@@ -19,33 +19,60 @@ module.exports = function (app) {
   app.route('/api/stock-prices')
     .get(function (req, res){
 
-       var stock =  req.query.stock;
-       var likes = req.query.likes || false;
-       var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-       ip = ip.split(',')[0];
+      var apiURL = "https://finance.google.com/finance/info?q=NASDAQ%3a";
+      var stockData = [];
 
-       console.log(stock);
+      var stock =  req.query.stock;
+      var likes = req.query.likes || false;
+      var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+      ip = ip.split(',')[0];
 
-       // if stock is array
-         // request first stock info and second
-         // search for likes in db
-         // bring stockdata together
-       // else 
+      console.log(stock);
 
-       request("https://finance.google.com/finance/info?q=NASDAQ%3a"+stock,
-        function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var data = JSON.parse(body.substring(3))[0];
-            var stockDoc = {
-              stockData: {
-                stock: data.t,
-                price: data.l,
-                likes: 0
-              }
-            }
-            res.json(stockDoc);
-          }
-        });
-    });
+      // if stock is array
+       // request first stock info and second
+       // search for likes in db
+       // if likes
+         // search likes for stock
+       // bring stockdata together
+      // else
+      // request api stock info
+      // request db like info
+      //
+
+      var setStockData = function(error, response, body){
+        if (!error && response.statusCode == 200) {
+          var data = JSON.parse(body.substring(3))[0];
+
+          stockData.push({
+             stock: data.t,
+             price: data.l,
+             likes: 0
+          });
+        }
+      };
+
+      var callbackTwo = function(error, response, body){
+        setStockData(error, response, body);
+        res.json({ stockData: stockData });
+      };
+
+      var callbackOne = function(error, response, body){
+        setStockData(error, response, body);
+        if(Array.isArray(stock)) {
+          request(apiURL+stock[1], callbackTwo);
+        } else {
+          res.json({ stockData: stockData });
+        }
+      }
+
+      if(Array.isArray(stock)) {
+        request(apiURL+stock[0], callbackOne);
+      } else {
+        request(apiURL+stock, callbackOne);
+      }
+
+
+    });// end get
 
 };
